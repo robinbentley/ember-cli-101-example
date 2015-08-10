92265f9d00f04ff5f617b06c24fd9b2ef599d14c
Robin Bentley <robinbentley@me.com>
2015-08-11 00:55:06 +0100

    Adding articles and nesting

--- app/models/friend.js
+++ app/models/friend.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  email: DS.attr('string'),
-  twitter: DS.attr('string'),
-  totalArticles: DS.attr('number'),
+  articles:       DS.hasMany('article'),
+  firstName:      DS.attr('string'),
+  lastName:       DS.attr('string'),
+  email:          DS.attr('string'),
+  twitter:        DS.attr('string'),
+  totalArticles:  DS.attr('number'),
   fullName: Ember.computed('firstName', 'lastName', {
     get() {
       return this.get('firstName') + ' ' + this.get('lastName');
